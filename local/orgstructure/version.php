8d5314ad188d8d304cbca39e50c9ea614cf30c5b
Ahmed Tofaha <ahmedtofaha999@gmail.com>
2025-04-27 00:27:40 +0300

    hotfix

--- local/orgstructure/version.php
+++ local/orgstructure/version.php
@@ -3,6 +3,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_orgstructure';
 $plugin->version = 2025042700;
-$plugin->requires = 2025042700;
+$plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
